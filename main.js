296b618cedd7b15815b0478eb479d19a21390722
Ripal Patel <ripal212007@gmail.com>
2018-09-05 22:47:10 -0500

    added javascript objects representing limited dictionary for hindi, spanish and italian language

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log("Hi");
+const hindiLang = {
+    merry:"merry",
+    christmas: "christmas",
+    and: "aur",
+    happy:"mubarak",
+    new:"naya",
+    year:"saal"
+};
+
+const spanishLang = {
+    merry:"feliz",
+    christmas:"navidad",
+    and:"y",
+    happy:"prospero",
+    new:"ano",
+    year:"nuevo"
+};
+
+const italianLang = {
+    merry :"buon",
+    christmas:"natale",
+    and:"e",
+    happy:"felice",
+    new:"anno",
+    year:"nuovo"
+};
