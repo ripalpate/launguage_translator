b0411395b802c359533995bf7e27f9f868503797
Ripal Patel <ripal212007@gmail.com>
2018-09-06 21:13:43 -0500

    modified dictionary with proper indentation

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-const hindiLang = {
-    merry:"merry",
-    christmas: "christmas",
-    and: "aur",
-    happy:"mubarak",
-    new:"naya",
-    year:"saal"
+let hindiLang = {
+    "merry": "merry",
+    "christmas": "christmas",
+    "and": "aur",
+    "happy": "mubarak",
+    "new": "naya",
+    "year": "saal"
 };
 
-const spanishLang = {
-    merry:"feliz",
-    christmas:"navidad",
-    and:"y",
-    happy:"prospero",
-    new:"ano",
-    year:"nuevo"
+let spanishLang = {
+    "merry": "feliz",
+    "christmas": "navidad",
+    "and": "y",
+    "happy": "prospero",
+    "new": "ano",
+    "year": "nuevo"
 };
 
-const italianLang = {
-    merry :"buon",
-    christmas:"natale",
-    and:"e",
-    happy:"felice",
-    new:"anno",
-    year:"nuovo"
+let italianLang = {
+    "merry": "buon",
+    "christmas": "natale",
+    "and": "e",
+    "happy": "felice",
+    "new": "anno",
+    "year": "nuovo"
 };
