b2a281059536889e798dbeddcb22bc9e0c56532e
Ripal Patel <ripal212007@gmail.com>
2018-09-08 13:37:24 -0500

    added condition for not found word.

--- main.js
+++ main.js
@@ -32,33 +32,45 @@ const printToDom = (stringToPrint, divId) => {
 
 let outputStringHindi = '';
 const newStringBuilderHindiLang = () => {
+    outputStringHindi = ' ';
     let inputString1 = document.getElementById('input_textarea');
     let inputString = inputString1.value.toLowerCase();
     let inputStringArray = inputString.split(' ');
     for (let i = 0; i < inputStringArray.length; i++) {
         outputStringHindi += `  ${hindiLang[inputStringArray[i]]}`;
+        if (!hindiLang[inputStringArray[i]]){
+            alert('Please try different message. words not found');
+        }
     }
     printToDom(outputStringHindi, 'output_textarea');
 }
 
 let outputStringSpanish = '';
 const newStringBuilderSpanishLang = () => {
+    outputStringSpanish = ' ';
     let inputString1 = document.getElementById('input_textarea');
     let inputString = inputString1.value.toLowerCase();
     let inputStringArray = inputString.split(' ');
     for (let i= 0; i < inputStringArray.length; i++) {
         outputStringSpanish += `  ${spanishLang[inputStringArray[i]]} `;
+        if (!hindiLang[inputStringArray[i]]){
+            alert('Please try different message. words not found');
+        }
     }
     printToDom(outputStringSpanish, 'output_textarea');   
 }
 
 let outputStringItalian = '';
 const newStringBuilderItalianLang = () => {
+    outputStringItalian = ' ';
     let inputString1 = document.getElementById('input_textarea');
     let inputString = inputString1.value.toLowerCase();
     let inputStringArray = inputString.split(' ');
     for (let i= 0; i < inputStringArray.length; i++) {
         outputStringItalian += `  ${italianLang[inputStringArray[i]]} `;
+        if (!hindiLang[inputStringArray[i]]){
+            alert('Please try different message. words not found');
+        }
     }
     printToDom(outputStringItalian, 'output_textarea');    
 }
